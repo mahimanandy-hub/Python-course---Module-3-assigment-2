9c02e1dcfebbc74c5bbbf0c87400c01394a6c33e
mahimanandy-hub <mn2676@srmist.edu.in>
2025-12-30 23:44:49 +0530

    Create task1.py
    this file detct odd and even number

---
+++ task1.py
@@ -0,0 +1,12 @@
+# Task 1: Check if a Number is Even or Odd
+
+
+num = int(input("Enter an number: "))
+
+if num % 2 == 0:
+        print(f"The number {num} is an Even number.")
+else:
+        print(f"The number {num} is an Odd number.")
+
+
+
